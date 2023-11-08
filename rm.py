390eb79724c240b6414441e5c44f1e45ec06d7de
Travis Tran <ttran2412@sdsu.edu>
2023-11-08 11:37:37 -0800

    updated rm.py

--- rm.py
+++ rm.py
@@ -1,21 +1,45 @@
 import math
-from utils import Task, ScheduleData, parse_input_file
+from utils import Task, ScheduleData, ScheduleBlock, CLOCK_STATE_TO_FREQ_MAP
+
+def next(data: ScheduleData):
+    #prioritize by lowest period
+    next_lowest_period = None
+    next_period = None
+    for t in data.tasks:
+        if next_lowest_period == None:
+            next_lowest_period = t.period
+        else:
+            if (t.complete == False and t.period < next_lowest_period):
+                next_period = t
+    
+    return next_period
 
 def rm_schedule(data: ScheduleData):
-    for i in range(1, data.task_count):
-        hyperperiod = data.tasks[0].period
-        hyperperiod = math.lcm(hyperperiod, data.tasks[0].period)
+    hyperperiod = data.exec_time
+    sched_vector: list[ScheduleBlock] = []
 
-    current_time = 0
-    for i in range(0, hyperperiod):
+    #create scheduling vector
+    for current_time in range(0, hyperperiod):
+        next_Task = next(data)
+        if next_Task is not None:
+            next_Task.complete = 1.0/next_Task.wcet_by_clock_state[0]
+
+            if next_Task.complete >= 1.0:
+                next_Task.percent_complete = 1
+                next_Task.complete = True
+
+            sched_vector.append(ScheduleBlock(next_Task.name, next_Task.power_by_clock_state[0], idle = False))
+            print("Append block Task")
+        else:
+            sched_vector.append(ScheduleBlock("IDLE", next_Task.power_by_clock_state[4], idle = True))
+            print("Append Block Idle")
         
-    
-    #print(f'{time started} {task name} {CPU frequency} {how long it ran for} {energy consumed in joules}J)
+        for t in data.tasks:
+            if current_time >= t.next_deadline:
+                t.next_deadline = t.period + current_time
+                t.complete = False
+                t.complete = 0.0
 
+    #iterate through scheduling vector
 
-def rm_scheduleEE(data: ScheduleData):
-    for i in range(1, data.task_count):
-        hyperperiod = data.tasks[0].period
-        hyperperiod = math.lcm(hyperperiod, data.tasks[0].period)
-
-    current_time = 0
+#print(f'{current_time} {data.task[0].name} {data.CPU frequency} {how long it ran for} {energy consumed in joules}J)
